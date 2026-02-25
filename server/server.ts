d87363437e35c83e136a6af4898bacc62d225b84
Thepranaypatil <patilpranayy@gmail.com>
2026-02-25 14:34:21 +0530

    Update server.ts

--- server/server.ts
+++ server/server.ts
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5174",
+    origin: "https://genetrix-ai-xlmj.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
